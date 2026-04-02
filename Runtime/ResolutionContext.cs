a993ec165a6cc3c4ca4aafbd47d0dc8d857e8b47
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-04-02 22:53:40 +0100

    Fix prefab resolution not falling back correctly
    Cleaned up DIContainer implementation as well

---
+++ Runtime/ResolutionContext.cs
@@ -0,0 +1,14 @@
+﻿namespace RPGFramework.DI
+{
+    public readonly struct ResolutionContext
+    {
+        public readonly IDIContainer Container;
+        public readonly IDIResolver  Resolver;
+
+        public ResolutionContext(IDIContainer container, IDIResolver resolver)
+        {
+            Container = container;
+            Resolver  = resolver;
+        }
+    }
+}
